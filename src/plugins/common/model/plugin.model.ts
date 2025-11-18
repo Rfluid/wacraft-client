5f10c59e01b8bff3faaa25b79c7e18926ba2d2a3
Rfluid <ruy.vieiraneto@gmail.com>
2025-11-18 19:00:20 -0300

    fix: plugin files

--- src/plugins/common/model/plugin.model.ts
+++ src/plugins/common/model/plugin.model.ts
@@ -1,5 +1,7 @@
+import { PluginExampleModule } from "../../example/example.module";
+
 export interface Plugin {
     name: string;
     path: string; // base path in router
-    moduleLoader: () => Promise<any>; // for loadChildren
+    moduleLoader: () => Promise<PluginExampleModule>; // for loadChildren
 }
