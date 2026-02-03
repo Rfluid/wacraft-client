c2fe176616e9f0239c30e94c0ef17979154c9a75
Ruy Vieira <ruy.vieiraneto@gmail.com>
2026-02-03 13:56:47 -0300

    feature: workspace ID as query param

--- src/core/navigation/service/query-params.service.ts
+++ src/core/navigation/service/query-params.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, inject } from "@angular/core";
 import { Router } from "@angular/router";
+import { WorkspaceContextService } from "../../workspace/store/workspace-context.service";
 
 @Injectable({
     providedIn: "root",
 })
 export class QueryParamsService {
     private router = inject(Router);
+    private workspaceContext = inject(WorkspaceContextService);
 
     // Global query params
     public sidebarOpen = false; // Handled by query params.
@@ -31,6 +33,7 @@ export class QueryParamsService {
     get globalQueryParams() {
         return {
             sidebar_open: this.sidebarOpen,
+            "workspace.id": this.workspaceContext.currentWorkspaceId,
         };
     }
 }
