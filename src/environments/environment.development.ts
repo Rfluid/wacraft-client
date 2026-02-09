39dedae1f229856fe16bc356fbda40bbdfef19b1
Ruy Vieira <ruy.vieiraneto@gmail.com>
2026-02-08 22:33:48 -0300

    fix: environment default variables

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -6,9 +6,8 @@ export const environment: EnvironmentConfig = {
     env: "development",
 
     mainServerUrl: "localhost:6900",
-    mainServerSecurity: true,
-    automationServerUrl: "localhost:1880",
-    automationServerSecurity: true,
+    mainServerSecurity: false,
+    automationServerSecurity: false,
     googleMapsApiKey: undefined, // Set your Google Maps API key here (should allow Google Places API and Google Maps JavaScript API)
     appTitle: "wacraft",
 
