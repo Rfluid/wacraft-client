5884b047d487186d95dc905942225773cb138492
Ruy Vieira <ruy.vieiraneto@gmail.com>
2026-04-09 17:27:40 -0300

    chore: lint and format

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe("AppComponent", () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector("h1")?.textContent || "Hello, wacraft-client").toContain("Hello, wacraft-client");
+        expect(compiled.querySelector("h1")?.textContent || "Hello, wacraft-client").toContain(
+            "Hello, wacraft-client",
+        );
     });
 });
